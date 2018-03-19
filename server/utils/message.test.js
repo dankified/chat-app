33de6b6c22fcd921c3bb2a54ae1f5348f879df67
Patricio de la Guardia <padlgt01@gmail.com>
2018-03-18 20:03:32 -0400

    Add basic styling and webpack config

--- server/utils/message.test.js
+++ server/utils/message.test.js
@@ -1,26 +1,28 @@
-var expect = require('expect');
+var expect = require("expect");
 
-var {generateMessage, generateLocationMessage} = require('./message');
+var { generateMessage, generateLocationMessage } = require("./message");
 
-describe('generateMessage', () => {
-  it('should generate correct message object', () => {
-    var from = "Pat";
+describe("generateMessage", () => {
+  it("should generate correct message object", () => {
+    var from = "Admin";
     var text = "Testing";
-    var message = generateMessage("Pat", text);
+    var message = generateMessage(from, text);
 
-    expect(message).toInclude({from, text});
-    expect(message.createdAt).toBeA('number');
+    expect(message).toInclude({ from, text });
+    expect(message.createdAt).toBeA("number");
   });
 });
 
-describe('generateLocationMessage', () => {
-  it('should generate correct location object', () => {
+describe("generateLocationMessage", () => {
+  it("should generate correct location object", () => {
     var latitude = 1;
     var longitude = 10;
     var from = "Admin";
     var message = generateLocationMessage(from, latitude, longitude);
 
-    expect(message.url).toBe(`https://www.google.com/maps?q=${latitude},${longitude}`);
-    expect(message).toInclude({from});
-  })
-})
+    expect(message.url).toBe(
+      `https://www.google.com/maps?q=${latitude},${longitude}`
+    );
+    expect(message).toInclude({ from });
+  });
+});
---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require("path");
+
+module.exports = {
+  entry: ["./public/js/index.js", "./public/css/styles.css"],
+  output: {
+    path: path.resolve(__dirname, "public"),
+    filename: "dist.js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js?$/,
+        exclude: path.resolve(__dirname, "node_modules"),
+        loader: "babel-loader",
+        options: {
+          presets: ["env"]
+        }
+      },
+      {
+        test: /\.css$/,
+        exclude: path.resolve(__dirname, "node_modules"),
+        loader: ["style-loader", "css-loader"]
+      }
+    ]
+  }
+} 
